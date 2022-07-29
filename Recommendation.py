bda1852ed7d6251d67060d4525d96798516c5a56
iosifgogolos <82125428+iosifgogolos@users.noreply.github.com>
2022-07-29 15:50:38 +0200

    Update Recommendation.py
    Printing the ratings for the recommendation engine in Python 3

--- Recommendation.py
+++ Recommendation.py
@@ -1,1 +1,11 @@
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
 
+i=0
+rating = PlayListRatings[0]
+
+while(rating > 6):
+    print(rating)
+    i=i+1
+    rating = PlayListRatings[i]
+    
+print("It took ", i ,"repetitions to get out of the loop.")
